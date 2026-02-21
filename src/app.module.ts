4da82f22989fbb201cb3cc2d178bc44d2c358fc2
João Pedro imperador <122656071+00joaopedro@users.noreply.github.com>
2026-02-21 01:09:32 -0300

    Refactor ServeStaticModule configuration in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,26 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    // Serve arquivos estáticos de /public (na raiz do projeto)
+    // Serve o frontend em /public (ex.: /public/index.html)
     ServeStaticModule.forRoot({
-  rootPath: join(__dirname, '..', 'public'),
-  serveRoot: '/public',
-  exclude: ['/api*'],
-}), ],
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/public',
+      // Se você NÃO usa app.setGlobalPrefix('api'), pode remover esse exclude.
+      exclude: ['/api*'],
+    }),
+
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
