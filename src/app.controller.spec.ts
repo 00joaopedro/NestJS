1a2668b0f7ef38f8026173037e6d20a65b58aa23
João Pedro imperador <122656071+00joaopedro@users.noreply.github.com>
2026-02-21 01:12:07 -0300

    Refactor tests to use AppController instead of AppService

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,18 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-describe('AppService', () => {
-  let service: AppService;
+describe('AppController', () => {
+  let controller: AppController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
       providers: [AppService],
     }).compile();
 
-    service = module.get<AppService>(AppService);
+    controller = module.get<AppController>(AppController);
   });
 
-  it('should return "Hello World!"', () => {
-    expect(service.getHello()).toBe('Hello World!');
+  it('health should return ok', () => {
+    expect(controller.health()).toEqual({ ok: true });
   });
 });
