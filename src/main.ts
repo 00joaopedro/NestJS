bf696ef10e8f30cb5d1f7de42b218ddc65468891
João Pedro imperador <122656071+00joaopedro@users.noreply.github.com>
2026-02-28 18:10:30 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
-
-  // Para testes: libera qualquer origin. Em produção, restrinja ao seu domínio.
   app.enableCors({ origin: true, credentials: true });
-
-  // opcional: app.setGlobalPrefix('api')
 
   const port = Number(process.env.PORT) || 3000;
   await app.listen(port, '0.0.0.0');
