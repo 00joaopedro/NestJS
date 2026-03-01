2cee4d8b3fd9f7b8f71779bcae4c264da1ff0484
João Pedro imperador <122656071+00joaopedro@users.noreply.github.com>
2026-03-01 00:27:29 -0300

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 
 import { SupabaseModule } from '../supabase/supabase.module';
+import { PrismaModule } from '../prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [SupabaseModule, PassportModule],
+  imports: [SupabaseModule, PrismaModule, PassportModule],
   providers: [JwtStrategy],
   controllers: [AuthController],
 })
